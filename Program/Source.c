0a43c2cd2538f93e61979c5b687cc49140cab236
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-04-15 21:44:46 +0900

    (2025.4.15) 실수를 저장하는 방법

--- Program/Source.c
+++ Program/Source.c
@@ -2,107 +2,64 @@
 
 void main()
 {
-#pragma region 산술 연산자
+#pragma region 오버플로우
+	// 특정한 자료형이 표현할 수 있는 최댓값의 범위를
+	// 넘어서 연산을 수행하는 과정입니다.
 
-	// int storage1 = 0;
-	// int storage2 = 0;
-	// int storage3 = 0;
-	// int storage4 = 0;
-	// int storage5 = 0;
+	// char storage = 127 + 1;
 	// 
-	// int x = 10;
-	// int y = 5;
-	// int z = 1;
-	// int w = 10;
+	// printf("storage의 값 : %d\n", storage);
+
+	// 오버플로우는 부호 없는 자료형에서도 똑같이 발생하며,
+	// 실수일 때 오버플로우가 발생하면 infinity라는 값으로 출력합니다.
+#pragma endregion
+
+#pragma region 언더플로우
+	// 특정한 자료형이 표현할 수 있는 최솟값을 범위를
+	// 넘어서 연산을 수행하는 과정입니다.
+
+	// char data = -128 - 1;
 	// 
-	// const int constant = 5;
+	// printf("data 변수의 값 : %d\n", data);
+
+	// 언더플로우는 부호 없는 자료형에서도 똑같이 발생하며,
+	// 실수일 때 언더플로우가 발생하면 0이라는 값으로 출력합니다.
+#pragma endregion
+
+#pragma region 부호 없는 자료형
+
+	// unsigned int count = 4294967295;
 	// 
-	// // 1. 변수 = 변수 + 변수
-	// storage1 = x + y;
+	// // [1111 1111] [1111 1111] [1111 1111] [1111 1111] 
+	// printf("sigend count 변수의 값 : %d\n", count);
 	// 
-	// // 2. 변수 = 변수 - 리터럴 상수
-	// storage2 = z - 1;
-	// 
-	// // 3. 변수 = 변수 * 심볼릭 상수
-	// storage3 = w * constant;
-	//  
-	// // 4. 변수 = 리터럴 상수 / 리터럴 상수
-	// storage4 = 20 / 5;
-	// 
-	// // 5. 변수 = 심볼릭 상수 % 리터럴 상수
-	// storage5 = constant % 5;
-	// 
-	// printf("storage1의 값 : %d\n", storage1);
-	// printf("storage2의 값 : %d\n", storage2);
-	// printf("storage3의 값 : %d\n", storage3);
-	// printf("storage4의 값 : %d\n", storage4);
-	// printf("storage5의 값 : %d\n", storage5);
+	// // [1111 1111] [1111 1111] [1111 1111] [1111 1111] 
+	// printf("unsigned count 변수의 값 : %u\n", count);
 
 #pragma endregion
 
-#pragma region 비트
-	// 데이터를 나타내는 최소의 단위이며, 0 또는 1의
-	// 조합으로 논리 계산을 하는 단위입니다.
+#pragma region 실수를 표현하는 방법
+	// float decimal = 3.333333f;
 
-#pragma region (10)진수를 (2)진수로 변환하는 과정
-	// 10진수를 1이 될 때까지 계속 2로 나누어 준 다음
-	// 나눈 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
+	// 정수형 리터럴 상수는 저장하는 값의 범위가 크지 않으면
+	// int 자료형으로 저장되며, 실수형 리터럴 상수는 double
+	// 자료형으로 저장됩니다.
 
-	// ex) 23 -> 10111
+	// printf("decimal 변수의 값 : %.10f\n", decimal);
+
+	// 표준 출력 함수로 소수를 출력할 때 4 byte 크기의 실수형
+	// 자료형은 소수점 이하 6자리까지만 표기하며, 8 byte 크기의
+	// 실수형 자료형은 소수점 이하 15자리까지만 표기합니다.
 #pragma endregion
 
-#pragma region (2)진수를 (10)진수로 변환하는 과정
-	// 1 byte에 2진수로 저장된 값을 2의 제곱으로 나타냅니다.
-
-	// 각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한
-	// 다음 각각의 비트를 모두 더하여 10진수로 나타냅니다.
-#pragma endregion
-
-#pragma region 비트 연산자
-	// 비트 단위로 논리 연산을 수행하기 위해 사용하는 연산자입니다.
-
-#pragma region AND 연산자
-	// 두 개의 피연산자가 모두 1이면 1을 반환하는 연산자입니다.
-
-	// int storage1 = 10; // 0000 1010
-	// int storage2 = 6;  // 0000 0110
-	//                    // ---------
-	//                    // 0000 0010
-	// 
-	// printf("storage1 & storage2 : %d\n", storage1 & storage2);
-
-#pragma endregion
-
-#pragma region OR 연산자
-	// 두 개의 피연산자 중에 하나라도 1이 있다면 1을 반환하는 연산자입니다.
-
-	// int storage3 = 8;  // 0000 1000
-	// int storage4 = 12; // 0000 1100
-	//                    // ---------
-	//                    // 0000 1100
-	// 
-	// printf("storage3 | storage4 : %d\n", storage3 | storage4);
-
-#pragma endregion
-
-#pragma region XOR 연산자
-	// 두 개의 피연산자가 서로 같으면 0을 반환하고, 서로 다르면
-	// 1을 반환하는 연산자입니다.
+#pragma region 실수 형태의 (10)진수를 (2)진수로 변환하는 과정
+	// 10진수 실수 부분을 1.0이 될 때까지 계속 2로 곱한 다음  
+	// 곱한 결과의 정수 부분을 위에서 아래로 순서대로 정렬합니다.
 
 
 
 #pragma endregion
 
-
-
-#pragma endregion
-
-
-
-
-	// 메모리는 비트 단위로 데이터를 저장할 수 있으며,
-	// 1개의 비트에는 0 또는 1의 값만 저장할 수 있습니다.
-#pragma endregion
 
 
 }
