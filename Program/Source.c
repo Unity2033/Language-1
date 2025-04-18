237aee5938f53c351f35db112ecda6f336231f01
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-04-18 21:43:41 +0900

    (2025.4.18) 자료형 변환

--- Program/Source.c
+++ Program/Source.c
@@ -2,56 +2,40 @@
 
 void main()
 {
-#pragma region 반복문
-	// 프로그램 내에서 특정한 작업을 반복적으로
-	// 수행하는 명령문입니다.
+#pragma region 자료형 변환
+	// 서로 다른 자료형을 가지고 있는 변수끼리 연산이
+	// 이루어질 때 기존에 지정했던 자료형을 다른 자료형
+	// 으로 변환하는 과정입니다.
 
-#pragma region 증감 연산자
-	// 피연산자를 하나씩 증가시키거나 감소시킬 때 
-	// 사용하는 연산자입니다.
+#pragma region 암묵적 자료형 변환
+	// 서로 다른 자료형으로 연산이 이루어질 때 자료형의
+	// 크기가 더 큰 자료형으로 변환되는 과정입니다.
 
-	// int code = 0;
+	// int integer = 10;
+	// float decimal = 5.5f;
 	// 
-	// int packet = ++code;
+	// float storage = integer + decimal;
 	// 
-	// printf("code 변수를 전위 증가한 값 : %d\n", code);
-	// printf("packet 변수의 값 : %d\n", packet);
-	// 
-	// packet = --code;
-	// 
-	// printf("code 변수를 전위 감소한 값 : %d\n", code);
-	// printf("packet 변수의 값 : %d\n", packet);
+	// printf("storage 변수의 값 : %f\n", storage);
 
-	// 전위 증감 연산자는 변수의 값을 증감시킨 후에 
-	// 연산을 수행합니다
-
-	// int data = 0;
-	// 
-	// int storage = data++;
-	// 
-	// printf("data 변수를 후위 증가한 값 : %d\n", data);
-	// printf("storage 변수의 값 : %d\n", storage);
-	// 
-	// storage = data--;
-	// 
-	// printf("data 변수를 후위 감소한 값 : %d\n", data);
-	// printf("storage 변수의 값 : %d\n", storage);
-	
-	// 후위 증감 연산자는 연산을 수행한 다음 변수의 값을 증감시킵니다.
+	// 표현 범위가 작은 데이터에 표현 범위가 큰 데이터를
+	// 저장하게 되면 암묵적으로 데이터 손실이 발생합니다.
 #pragma endregion
 
-#pragma region for문
-	// 초기식을 연산하여 조건식의 결과에 따라 특정한
-	// 횟수만큼 반복하는 반복문입니다.
+#pragma region 명시적 자료형 변환
+	// 연산이 이루어지기 전에 사용자가 직접 자료형을
+	// 변환하는 과정입니다.
 
-	//	for (int i = 0; i < 5; i++)
-	//	{
-	//		printf("update\n");
-	//	}
+	// int critical = 10;
+	// int penetration = 3;
+	// 
+	// float attack = (float)critical / penetration;
+	// 
+	// printf("attack 변수의 값 : %f\n", attack);
 
+	// 정수형 변수끼리 연산을 수행하게되면 정수의 결과
+	// 값만 가질 수 있습니다.
 #pragma endregion
-
-
 
 #pragma endregion
 
